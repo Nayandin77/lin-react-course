0209e1157c22f5c7009c8cd557a56e75e12b538d
Nayandin77 <nayandindennis@yahoo.com>
2020-01-19 19:04:03 -0500

    removed constructor and made simple state variable in Library Class

--- src/index.js
+++ src/index.js
@@ -1,12 +1,116 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React, { Component } from 'react'
+import { render } from 'react-dom'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+let bookList = [
+    {"title": "Rando Book 1", "author": "Dennis Nayandin", "pages":250},
+    {"title": "Rando Book 2", "author": "Dennis Nayandin", "pages":260},
+    {"title": "Rando Book 3", "author": "Dennis Nayandin", "pages":480},
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+]
+
+const Book = ({title, author, pages}) => {
+    return (
+        <section>
+            <h2>{title}</h2>
+            <p>by: {author}</p>
+            <p>Pages: {pages} page</p>
+        </section>
+    )
+}
+
+class Library extends React.Component {
+
+    state = { open: false }
+    
+    toggleOpenClosed = () => {
+        this.setState(prevState => ({
+            open: !prevState.open
+        }))
+    }
+
+    render() {
+        const { books } = this.props
+        return (
+            <div>
+                <h1>Welcome to the Library, we are {this.state.open ? 'open' : 'closed'}!</h1>
+                <button onClick={this.toggleOpenClosed}>Change</button>
+                {books.map(
+                    (book, i) => 
+                        <Book
+                            key={i}
+                            title={book.title} 
+                            author={book.author} 
+                            pages={book.pages} />
+    
+                )}
+            </div>
+        )
+    }
+    
+}
+
+
+render(
+    <Library books={bookList}/>
+    ,
+    document.getElementById('root')
+)
+
+// let skiData = {
+//     total: 50,
+//     powder: 20,
+//     backcountry: 10,
+//     goal: 100
+// }
+
+// const getPercent = decimal => {
+//     return decimal * 100 + '%'
+// }
+// const calcGoalProgress = (total, goal) => {
+//     return getPercent(total/goal)
+// }
+
+// const SkiDayCounter = ({total, powder, backcountry, goal}) => {
+//     return(
+//         <section>
+//             <div>
+//                 <p>Total Days: {total}</p>
+//             </div>
+//             <div>
+//                 <p>Powder Days: {powder}</p>
+//             </div>
+//             <div>
+//                 <p>Backcountry Days: {backcountry}</p>
+//             </div>
+//             <div>
+//                 <p>Goal Progress: {calcGoalProgress(total, goal)}</p>
+//             </div>
+//         </section>
+//     )
+// }
+
+
+// render(
+//     <SkiDayCounter 
+//         total={skiData.total}
+//         powder={skiData.powder}
+//         backcountry={skiData.backcountry}
+//         goal={skiData.goal}
+//     />
+//     //<Message color="blue" age={22} msg="how are you?"/>
+//     ,
+//     document.getElementById('root')
+// )
+
+// class Message extends React.Component {
+//     render() {
+//         return (
+//             <div>
+//                 <h1 style={{color: this.props.color}}>
+//                     {this.props.msg}
+//                 </h1>
+//                 <p>Test number: {this.props.age} minutes</p>
+//             </div>
+//         )
+//     }
+// }
