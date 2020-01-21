1d5f72fffcc73a122674ca5030c35fba018766bb
Nayandin77 <nayandindennis@yahoo.com>
2020-01-21 11:20:38 -0500

    broke down each component into separate file

--- src/index.js
+++ src/index.js
@@ -1,103 +1,14 @@
 import React from 'react'
 import { render } from 'react-dom'
-import PropTypes from 'prop-types'
+import Library from './Library'
 
+// data
 let bookList = [
     {"title": "Rando Book 1", "author": "Dennis Nayandin", "pages":250},
     {"title": "Rando Book 2", "author": "Dennis Nayandin", "pages":260},
     {"title": "Rando Book 3", "author": "Dennis Nayandin", "pages":480},
 
 ]
-
-const Book = ({title="No Title Provided", author="No Author", pages=0, freeBookmark}) => {
-    return (
-        <section>
-            <h2>{title}</h2>
-            <p>by: {author}</p>
-            <p>Pages: {pages} page</p>
-            <p>Free Bookmark Today: {freeBookmark ? 'yes!': 'no!'}</p>
-        </section>
-    )
-}
-
-const Hiring = () => 
-    <div>
-        <p>The Library is hiring. Go to www.library.com/jobs for more.</p>
-    </div>
-
-const NotHiring = () => 
-    <div>
-        <p>The Library is not hiring.</p>
-    </div>
-
-class Library extends React.Component {
-
-    static defaultProps = {
-        books: [
-            {"title": "Tahoe Tales", "author": "Chet Whitley", "pages":1000}
-        ]
-    }
-
-    state = { 
-        open: false ,
-        freeBookmark: true,
-        hiring: true,
-        data: [],
-        loading: false
-    }
-
-    componentDidMount() {
-        this.setState({loading: true})
-        fetch('https://hplussport.com/api/products/order/price/sort/asc/qty/1')
-            .then(data => data.json())
-            .then(data => this.setState({data, loading: false}))
-    }
-
-    componentDidUpdate() {
-        console.log("updated")
-    }
-    
-    toggleOpenClosed = () => {
-        this.setState(prevState => ({
-            open: !prevState.open
-        }))
-    }
-
-    render() {
-        const { books } = this.props
-        return (
-            <div>
-                <h1>Welcome to the Library, we are {this.state.open ? 'open' : 'closed'}!</h1>
-                {this.state.loading
-                    ? "loading..."
-                    : <div>
-                        {this.state.data.map(product => {
-                            return (
-                                <div key={product.id}>
-                                    <h3>Library Product of the Week!</h3>
-                                    <h4>{product.name}</h4>
-                                    <img alt={product.name} src={product.image} height={100} />
-                                </div>
-                            )
-                        })}
-                    </div>}
-                {this.state.hiring ? <Hiring /> : <NotHiring />}
-                <button onClick={this.toggleOpenClosed}>Change</button>
-                {books.map(
-                    (book, i) => 
-                        <Book
-                            key={i}
-                            title={book.title} 
-                            author={book.author} 
-                            pages={book.pages}
-                            freeBookmark={this.state.freeBookmark} />
-    
-                )}
-            </div>
-        )
-    }
-    
-}
 
 // class FavoriteColorForm extends React.Component {
 //     state = { value: ''}
@@ -125,16 +36,7 @@ class Library extends React.Component {
 //     }
 // }
 
-Library.propTypes = {
-    books: PropTypes.array
-}
 
-Book.propTypes = {
-    title: PropTypes.string,
-    author: PropTypes.string,
-    pages: PropTypes.number,
-    freeBookmark: PropTypes.bool
-}
 
 render(
     <Library books={bookList} />
